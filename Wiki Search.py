d142f5d3d278a4d77a0bb6d079bda477292adbd7
Yunis Huseynzade <32550502+YHProgramming@users.noreply.github.com>
2017-10-05 21:09:09 +0400

    Add files via upload

---
+++ Wiki Search.py
@@ -0,0 +1,8 @@
+"""
+Please Subscribe me on Youtube : https://www.youtube.com/channel/UCPn6gQZ9ySWq6RXpiE4zNeg
+"""
+import webbrowser
+lib = raw_input("Search:")
+url = "https://en.wikipedia.org/wiki/" +(str(lib))+ "_"
+webbrowser.open_new(url)
+webbrowser.open_new("http://yhprogram.tr.gg/")
