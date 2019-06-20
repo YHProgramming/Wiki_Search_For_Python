c7dd2530dc50753888a1b63c8ab201f429da4166
Yunis Huseynzade <32550502+YunisHuseynzade@users.noreply.github.com>
2019-06-20 22:01:58 +0300

    Update Wiki Search.py

--- Wiki Search.py
+++ Wiki Search.py
@@ -1,6 +1,3 @@
-"""
-Please Subscribe me on Youtube : https://www.youtube.com/channel/UCPn6gQZ9ySWq6RXpiE4zNeg
-"""
 import webbrowser
 lib = raw_input("Search:")
 url = "https://en.wikipedia.org/wiki/" +(str(lib))+ "_"
